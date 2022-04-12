1ee21f41558fe9b7318d1e254d8d1587747b9403
jrfm <moesch.jonas@protonmail.com>
2022-04-12 15:26:15 +0200

    add computerPlay function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+function computerPlay() {
+    // define array with the different game elements
+    const elements = ["rock", "paper", "scissors"];
+    // create random number between 0 and 3
+    let randomElement = Math.floor(Math.random() * 3);
+    // element choosen by computer
+    choosenElement = elements.at(randomElement);
+    return choosenElement
+}
+
+
