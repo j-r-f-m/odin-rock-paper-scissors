d5c92956ef89e7f7139dae3f02820fb7713166c3
jrfm <moesch.jonas@protonmail.com>
2022-04-12 15:51:26 +0200

    add playRound function

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ function computerPlay() {
 }
 
 
+function playRound(playerSelection, computerSelection) {
+    // convert player input to lower case
+    lowerCasePlayerSelection = playerSelection.toLowerCase()
+    console.log(lowerCasePlayerSelection)
+    // player winning conditions
+    if (lowerCasePlayerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'Rock beats Scissors. You win!'
+    } else if (lowerCasePlayerSelection == 'scissors' && computerSelection == 'paper') {
+        return 'Scissors beats Paper. You win!'
+    } else if (lowerCasePlayerSelection == 'paper' && computerSelection == 'rock') {
+        return 'Paper beats Rock. You win!'
+    // computer winning conditions    
+    } else if (lowerCasePlayerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'Rock beats Scissors. You lose!'
+    } else if (lowerCasePlayerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'Scissors beats Paper. You lose!'
+    } else if (lowerCasePlayerSelection == 'rock' && computerSelection == 'paper') {
+        return 'Paper beats Rock. You lose!'
+    } else if (lowerCasePlayerSelection == computerSelection) {
+        return 'Draw! Play again?'
+    }
+}
+
+
+console.log(playRound('Rock', computerPlay()))
